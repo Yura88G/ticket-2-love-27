badb36f14882c7efbc07b35c73d309459a5bd4bf
Yura88G <380937998110ashotas@gmail.com>
2025-10-29 07:07:37 +0200

    Update profiles.js

--- js/profiles.js
+++ js/profiles.js
@@ -1,1 +1,93 @@
-
+// data/profiles.js
+const profiles = [
+    {
+        id: 1,
+        name: "Олександр",
+        age: 32,
+        city: "Київ",
+        gender: "men",
+        img: "men1.jpg",
+        description: "Люблю подорожі та спорт. Шукаю серйозні стосунки."
+    },
+    {
+        id: 2,
+        name: "Дмитро",
+        age: 28,
+        city: "Львів",
+        gender: "men",
+        img: "men2.jpg",
+        description: "Музикант, люблю каву і глибокі розмови."
+    },
+    {
+        id: 3,
+        name: "Анастасія",
+        age: 26,
+        city: "Одеса",
+        gender: "women",
+        img: "women1.jpg",
+        description: "Танцюристка, люблю море і романтику."
+    },
+    {
+        id: 4,
+        name: "Марія",
+        age: 29,
+        city: "Харків",
+        gender: "women",
+        img: "women2.jpg",
+        description: "Дизайнерка, шукаю натхнення і любов."
+    },
+    {
+        id: 5,
+        name: "Ігор",
+        age: 35,
+        city: "Дніпро",
+        gender: "men",
+        img: "men3.jpg",
+        description: "Бізнесмен, люблю активний відпочинок."
+    },
+    {
+        id: 6,
+        name: "Софія",
+        age: 24,
+        city: "Київ",
+        gender: "women",
+        img: "women3.jpg",
+        description: "Студентка, люблю книги і прогулянки."
+    },
+    {
+        id: 7,
+        name: "Віктор",
+        age: 30,
+        city: "Львів",
+        gender: "men",
+        img: "men4.jpg",
+        description: "Фотограф, шукаю свою музу."
+    },
+    {
+        id: 8,
+        name: "Олена",
+        age: 31,
+        city: "Одеса",
+        gender: "women",
+        img: "women4.jpg",
+        description: "Вчителька, люблю дітей і природу."
+    },
+    {
+        id: 9,
+        name: "Павло",
+        age: 33,
+        city: "Київ",
+        gender: "men",
+        img: "men5.jpg",
+        description: "IT-фахівець, люблю техніку і природу."
+    },
+    {
+        id: 10,
+        name: "Юлія",
+        age: 27,
+        city: "Львів",
+        gender: "women",
+        img: "women5.jpg",
+        description: "Фотографка, люблю мистецтво і подорожі."
+    }
+];
